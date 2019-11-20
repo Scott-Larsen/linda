42506c29de5227316065199ec9c7c30b116bf97d
Scott <scott@scottlarsen.com>
2019-11-20 00:01:59 -0800

    Change rel=noopener to rel=noreferrer

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,9 @@ About Linda
 
 <p>Linda Dove holds a Ph.D. in Renaissance literature and teaches college writing. She is also an award-winning poet, and her books include, <i>In Defense of Objects</i> (2009), <i>O Dear Deer,</i> (2011), <i>This Too</i> (2017), <i>Fearn</i> (2019) and the scholarly collection of essays, <i>Women, Writing, and the Reproduction of Culture in Tudor and Stuart Britain</i> (2000). Poems have been nominated for a Pushcart Prize, the Robert H. Winner Award from the Poetry Society of America, and Best of the Net. She lives with her human family, two Jack Russell terriers, and a small flock of chickens in the foothills east of Los Angeles, where she serves as the faculty editor of <i>MORIA</i> Literary Magazine at Woodbury University.</p>
 
-<a target="_blank" href="https://www.pw.org/directory/writers/linda_dove">Poets & Writers page</a></br>
-<a target="_blank" href="https://www.amazon.com/Linda-Dove/e/B0041NFIR2?ref=dbs_p_pbk_r00_abau_000000">Amazon author page</a></br>
-<a target="_blank" href="https://woodbury.edu/faculty/linda-dove">Woodbury University faculty page</a>
+<a target="_blank" rel="noreferrer" href="https://www.pw.org/directory/writers/linda_dove">Poets & Writers page</a></br>
+<a target="_blank" rel="noreferrer" href="https://www.amazon.com/Linda-Dove/e/B0041NFIR2?ref=dbs_p_pbk_r00_abau_000000">Amazon author page</a></br>
+<a target="_blank" rel="noreferrer" href="https://woodbury.edu/faculty/linda-dove">Woodbury University faculty page</a>
 
 </div>
 
